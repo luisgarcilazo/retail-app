8d0ccd94ed1e4b34da9cd96b1ae4a65de802f00f
luisgarcilazo <luis_david_garcilazo_medrano@hotmail.com>
2023-06-27 13:17:38 -0500

    added initial spring project

---
+++ retail-app/src/main/java/com/example/retailapp/RetailAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.retailapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailAppApplication.class, args);
+	}
+
+}
