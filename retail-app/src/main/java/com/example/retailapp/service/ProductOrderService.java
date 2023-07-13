493d0064308154828ca6d2c746790778145ce8e7
luisgarcilazo <luis_david_garcilazo_medrano@hotmail.com>
2023-07-13 16:01:42 -0500

    removed uploads to clean database tomorrow

--- retail-app/src/main/java/com/example/retailapp/service/ProductOrderService.java
+++
@@ -1,9 +0,0 @@
-package com.example.retailapp.service;
-
-import com.example.retailapp.entity.ProductOrder;
-
-public interface ProductOrderService {
-
-    //saves a product order into the database
-    ProductOrder save(ProductOrder productOrder);
-}
